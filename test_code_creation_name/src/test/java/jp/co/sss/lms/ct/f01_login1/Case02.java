de027fd25bfd2ad5291045f2aa118962359db98f
y-tamura <y-tamura@ISI-OA25L16>
2026-09-18 11:53:14 +0900

    add：Case02.java test01,02 ログイン画面遷移後、存在しないユーザーによるログインテストコード追加
    WebElementを利用しDBに登録されていないユーザーのログインIDとパスワードを入力し、エラーメッセージが表示されるか確認するテストを作成し、エビデンスを取得した。
    期待値は以下の通り。
    
    エラーメッセージ：* ログインに失敗しました。
    
    【入力値】
    ログインID：123456
    パスワード：123456

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -31,18 +34,42 @@ public class Case02 {
 		closeDriver();
 	}
 
+	/** test02-01　トップページへアクセスし、ログイン画面が表示される */
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
 	}
 
+	/** test02-02　DBに登録されていないユーザーでログインをし、エラーメッセージを表示 */
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms/");
+
+		//入力値の入力
+		WebElement id = webDriver.findElement(By.id("loginId"));
+		id.clear();
+		id.sendKeys("123456");
+
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("123456");
+
+		//ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//エラーメッセージの確認
+		WebElement errorMsg = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertEquals("* ログインに失敗しました。", errorMsg.getText());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }

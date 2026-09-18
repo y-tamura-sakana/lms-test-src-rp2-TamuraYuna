6c059a02786546a2bba63e856125b55a8c7b8ed6
y-tamura <y-tamura@ISI-OA25L16>
2026-09-18 16:11:24 +0900

    add：Case03 tesst01,02のログイン処理テストコードの追加とエビデンス取得
    WebElemntを活用し、初回ログイン済みの受講生ユーザーのログイン処理のテストコードを作成し、エビデンスを取得した。
    期待結果は以下のとおりである。
    
    ・ログイン画面へ遷移
    ・ログイン成功後、コース詳細画面へ遷移
    
    【入力値】
    ログインID：StudentAA01
    パスワード：StudentAA0101

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +21,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
 public class Case03 {
+
+	//トップ画面
+	final String topUrl = "http://localhost:8080/lms/";
+
+	//コース詳細画面
+	final String courseDetailUrl = "http://localhost:8080/lms/course/detail";
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +44,41 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		goTo(topUrl);
+
+		//ログイン画面遷移の確認処理
+		assertEquals(topUrl, webDriver.getCurrentUrl());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		goTo(topUrl);
+
+		//入力値の入力
+		WebElement id = webDriver.findElement(By.id("loginId"));
+		id.clear();
+		id.sendKeys("StudentAA01");
+
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("StudentAA0101");
+
+		//ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//画面遷移の確認
+		assertEquals(courseDetailUrl, webDriver.getCurrentUrl());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }

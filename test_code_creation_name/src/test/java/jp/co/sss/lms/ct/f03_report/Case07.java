364a4637f9bf4a05bc0731ba0dd78df82affc4e7
y-tamura <y-tamura@ISI-OA25L16>
2026-09-25 09:30:38 +0900

    add:Case07 テストコード作成とエビデンス取得
    初回ログイン済みの受講生ユーザーで日報登録のテストコードを作成し、エビデンスを取得した。
    期待結果は以下の通りである。
    ・ログイン画面へ遷移
    ・ログイン成功後、コース詳細画面へ遷移
    ・詳細ボタン押下後、セクション詳細画面へ遷移
    ・提出ボタンから日報登録画面へ遷移
    ・提出ボタンからセクション詳細画面へ遷移

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -18,6 +21,18 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース07 受講生 レポート新規登録(日報) 正常系")
 public class Case07 {
+
+	//トップ画面
+	final String topUrl = "http://localhost:8080/lms/";
+
+	//コース詳細画面
+	final String courseDetailUrl = "http://localhost:8080/lms/course/detail";
+
+	//セクション詳細画面
+	final String sectionUrl = "http://localhost:8080/lms/section/detail";
+
+	//レポート登録画面
+	final String reportRegistUrl = "http://localhost:8080/lms/report/regist";
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,35 +50,115 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo(topUrl);
+
+		//ログイン画面遷移の確認処理
+		assertEquals(topUrl, webDriver.getCurrentUrl());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//入力値の入力
+		WebElement id = webDriver.findElement(By.id("loginId"));
+		id.clear();
+		id.sendKeys("StudentAA01");
+
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("StudentAA0101");
+
+		//ログインボタン押下
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+		//画面遷移の確認
+		assertEquals(courseDetailUrl, webDriver.getCurrentUrl());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//画面の最大化
+		webDriver.manage().window().maximize();
+
+		//10/5の詳細ボタンを押下
+		String targetDate = "10月5日";
+		WebElement detailButton = webDriver
+				.findElement(By.xpath("//tr[td[contains(text(),'" + targetDate + "')]]//input[@value='詳細']"));
+		detailButton.click();
+
+		//セクション画面遷移の確認
+		assertEquals(sectionUrl, webDriver.getCurrentUrl());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		By submitPath = By.xpath("//input[@type='submit' and contains(@value, 'を提出する')]");
+
+		//待ち処理
+		visibilityTimeout(submitPath, 10);
+
+		//「日報【デモ】を提出する」を押下
+		WebElement submission = webDriver
+				.findElement(submitPath);
+		submission.click();
+
+		//レポート登録画面遷移の確認
+		assertEquals(reportRegistUrl, webDriver.getCurrentUrl());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+
+		//テキストエリアをクリック
+		WebElement inputText = webDriver.findElement(By.cssSelector("textarea"));
+		inputText.click();
+
+		//報告内容を入力
+		String Text = "今日はテストコード演習を行った。";
+		inputText.clear();
+		inputText.sendKeys(Text);
+
+		//エビデンス取得（入力）
+		String suffix = "01_報告内容の入力";
+		getEvidence(new Object() {
+		}, suffix);
+
+		//「提出する」ボタンを押下
+		webDriver.findElement(By.cssSelector("button[type='submit']")).click();
+
+		//セクション画面遷移の確認
+		assertTrue(webDriver.getCurrentUrl().startsWith(sectionUrl));
+
+		//エビデンスの取得（画面遷移）
+		suffix = "02_セクション画面への遷移";
+		getEvidence(new Object() {
+		}, suffix);
 	}
 
 }
